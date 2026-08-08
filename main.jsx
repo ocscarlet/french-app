f472f733ba4017e8ad4063324b3c9d1423746bd3
ocscarlet <ocscarlet@gmail.com>
2026-08-08 10:24:08 +0800

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FrenchLearningApp from './FrenchLearningApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FrenchLearningApp />
+  </React.StrictMode>
+)
